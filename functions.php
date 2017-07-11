a0ff16f5e2db200bdbca87e8b539ac02aab5bb47
C.S. Maggio <christinem@soundviewmed.com>
2017-07-11 15:17:48 -0700

    Replaced with the appropriate file
    I know, I know, I messed up.

--- functions.php
+++ functions.php
@@ -1,4 +1,19 @@
 <?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'storefront-style','storefront-style','storefront-icons','storefront-woocommerce-style' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css' );
+
+// END ENQUEUE PARENT ACTION
+
 // Add action to hook into the approp
 add_filter( 'woocommerce_placeholder_img_src', 'growdev_custom_woocommerce_placeholder', 10 );
 /**
@@ -8,77 +23,3 @@ function growdev_custom_woocommerce_placeholder( $image_url ) {
   $image_url = 'http://127.0.0.1:4001/wordpress/wp-content/uploads/2017/07/PlaceholderImage.png';  // change this to the URL to your custom placeholder
   return $image_url;
 }
-
-/**
- * Storefront engine room
- *
- * @package storefront
- */
-
-/**
- * Assign the Storefront version to a var
- */
-$theme              = wp_get_theme( 'storefront' );
-$storefront_version = $theme['Version'];
-
-/**
- * Set the content width based on the theme's design and stylesheet.
- */
-if ( ! isset( $content_width ) ) {
-	$content_width = 980; /* pixels */
-}
-
-$storefront = (object) array(
-	'version' => $storefront_version,
-
-	/**
-	 * Initialize all the things.
-	 */
-	'main'       => require 'inc/class-storefront.php',
-	'customizer' => require 'inc/customizer/class-storefront-customizer.php',
-);
-
-require 'inc/storefront-functions.php';
-require 'inc/storefront-template-hooks.php';
-require 'inc/storefront-template-functions.php';
-
-if ( class_exists( 'Jetpack' ) ) {
-	$storefront->jetpack = require 'inc/jetpack/class-storefront-jetpack.php';
-}
-
-if ( storefront_is_woocommerce_activated() ) {
-	$storefront->woocommerce = require 'inc/woocommerce/class-storefront-woocommerce.php';
-
-	require 'inc/woocommerce/storefront-woocommerce-template-hooks.php';
-	require 'inc/woocommerce/storefront-woocommerce-template-functions.php';
-}
-
-if ( is_admin() ) {
-	$storefront->admin = require 'inc/admin/class-storefront-admin.php';
-
-	require 'inc/admin/class-storefront-plugin-install.php';
-}
-
-/**
- * NUX
- * Only load if wp version is 4.7.3 or above because of this issue;
- * https://core.trac.wordpress.org/ticket/39610?cversion=1&cnum_hist=2
- */
-if ( version_compare( get_bloginfo( 'version' ), '4.7.3', '>=' ) && ( is_admin() || is_customize_preview() ) ) {
-	require 'inc/nux/class-storefront-nux-admin.php';
-	require 'inc/nux/class-storefront-nux-guided-tour.php';
-
-	if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '3.0.0', '>=' ) ) {
-		require 'inc/nux/class-storefront-nux-starter-content.php';
-	}
-}
-
-/**
- * Note: Do not add any custom code here. Please use a custom plugin so that your customizations aren't lost during updates.
- * https://github.com/woocommerce/theme-customisations
- */
-
-/**
- * Note: you can't tell me what to do, you're not my real mom
- */
-
